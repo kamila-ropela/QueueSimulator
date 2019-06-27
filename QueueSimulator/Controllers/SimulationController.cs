303eff15c5f0bc7acae82d752c3b2bb106419889
kamila-ropela <kamila.ropela@gmail.com>
2019-06-27 23:27:12 +0200

    View to add new patient

--- QueueSimulator/Controllers/SimulationController.cs
+++ QueueSimulator/Controllers/SimulationController.cs
@@ -13,6 +13,11 @@ namespace QueueSimulator.Controllers
             return View();
         }
 
+        public IActionResult AddPatient()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
