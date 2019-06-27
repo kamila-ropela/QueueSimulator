00f9814440f8eda5062d4d0dcf3cf26094881c71
kamila-ropela <kamila.ropela@gmail.com>
2019-06-27 19:28:15 +0200

    Create Patient.cs

---
+++ QueueSimulator/Models/Patient.cs
@@ -0,0 +1,9 @@
+﻿namespace QueueSimulator.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string PatientName { get; set; }
+        public string Status { get; set; }
+    }
+}
