03f0a4be773391d6915f85ec38fff894cec11c82
kamila-ropela <kamila.ropela@gmail.com>
2019-08-22 00:11:18 +0200

    Count priority of leaving patients from query

--- QueueSimulator/Simulation/SimulationProcess.cs
+++ QueueSimulator/Simulation/SimulationProcess.cs
@@ -22,7 +22,7 @@ namespace QueueSimulator.Simulation
         {
             for (int element = 0; element < 2 * Helper.doctorCount; element++)
             {
-                var priorityLeave = this.ChoosePatienToLeaveQuery();//random.Next(1, Helper.highestPriority + 1);
+                var priorityLeave = this.ChoosePatienToLeaveQuery();
                 DeletePatientFromTheList(priorityLeave);
             }
         }
@@ -32,30 +32,88 @@ namespace QueueSimulator.Simulation
             //kolejka z wyzszym priorytetem
             for (int element = 0; element < Helper.doctorCount; element++)
             {
-                var priorityLeave = random.Next(1, Helper.highestPriority + 1);
+                var priorityLeave = ChoosePatientToLeaveTwoQuery(4, 3);
                 DeletePatientFromTheList(priorityLeave);
             }
 
             //kkolejka z niższym priorytetem
             for (int element = 0; element < Helper.doctorCount; element++)
             {
-                var priorityLeave = random.Next(1, Helper.highestPriority / 2 + 1);
+                var priorityLeave = ChoosePatientToLeaveTwoQuery(2, 1);
                 DeletePatientFromTheList(priorityLeave);
             }
         }
 
+        private int[] ChangePriority(int[] patientPriorityGropu, int patientPriorityCount)
+        {
+            for (int i = 0; i < patientPriorityCount; i++)
+            {
+                if (patientPriorityGropu[i] == 1)
+                {
+                    patientPriorityGropu[i] = 4;
+                    continue;
+                }
+
+                else if (patientPriorityGropu[i] == 2)
+                {
+                    patientPriorityGropu[i] = 3;
+                    continue;
+                }
+                else if (patientPriorityGropu[i] == 3)
+                {
+                    patientPriorityGropu[i] = 2;
+                    continue;
+                }
+                else
+                {
+                    patientPriorityGropu[i] = 1;
+                    continue;
+                }
+            }
+
+            return patientPriorityGropu;
+        }
+
         private int ChoosePatienToLeaveQuery()
         {
-             var randomPriority = random.Next(1, 11);
+            var patientPriorityGropu = patientList.GroupBy(x => x.Priority).Select(x => x.Key).ToArray(); //podział na grupy
 
-            if (randomPriority <= 4)
+            patientPriorityGropu = ChangePriority(patientPriorityGropu, patientPriorityGropu.Count());
+            var patientPriorityOrder = patientPriorityGropu.OrderByDescending(x => x).Select(x => Convert.ToDouble(x)).Select(x => x/((double)patientPriorityGropu.Sum()));
+
+
+            var randomPriority = Math.Round((double)random.Next(1, patientPriorityGropu.Sum() + 1), 2)/(double)patientPriorityGropu.Sum();
+
+            if (randomPriority > 1 - patientPriorityOrder.ElementAt(0))
                 return 1;
-            else if (randomPriority == 10)
-                return 4;
-            else if (randomPriority == 8 || randomPriority == 9)
+            else if (randomPriority > 1 - patientPriorityOrder.ElementAt(0) - patientPriorityOrder.ElementAt(1))
                 return 2;
-            else
+            else if (randomPriority > 1 - patientPriorityOrder.ElementAt(0) + patientPriorityOrder.ElementAt(1) + patientPriorityOrder.ElementAt(2))
                 return 3;
+            else
+                return 4;
+        }
+
+        private int ChoosePatientToLeaveTwoQuery(int priorityMin, int priorityMax)
+        {
+            var patientPriorityGropu = patientList.GroupBy(x => x.Priority).Select(x => x.Key).Where(x => x.Equals(priorityMin) || x.Equals(priorityMax)).ToArray(); //podział na grupy
+
+            if (patientPriorityGropu.Count() != 1)
+            {
+                patientPriorityGropu = ChangePriority(patientPriorityGropu, patientPriorityGropu.Count());
+                var patientPriorityOrder = patientPriorityGropu.OrderByDescending(x => x).Select(x => Convert.ToDouble(x)).Select(x => x / ((double)patientPriorityGropu.Sum()));
+
+
+                var randomPriority = Math.Round((double)random.Next(1, patientPriorityGropu.Sum() + 1), 2) / (double)patientPriorityGropu.Sum();
+
+
+                if (randomPriority <= patientPriorityOrder.ElementAt(1))
+                    return priorityMin;
+                else
+                    return priorityMax;
+            }
+
+            return patientPriorityGropu[0];
         }
 
         //wypełnienie pacjetow na poczatku symulacji
